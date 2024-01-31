759a790f3bc3900c96dc6117af995257edc1bce3
aabruin <156900063+aabruin@users.noreply.github.com>
2024-01-30 19:43:31 -0800

    Add files via upload

--- aa_answers/24_30_01_Groundhog.R
+++ aa_answers/24_30_01_Groundhog.R
@@ -1,1 +1,5 @@
+## Read in datasets
+
+groundhogs <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-01-30/groundhogs.csv')
+predictions <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-01-30/predictions.csv')
 
